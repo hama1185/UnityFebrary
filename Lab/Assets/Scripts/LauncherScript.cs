a8c27b36f6528a77fff25f203efe50c2e98853c6
hama1185 <hamataku0120@gmail.com>
2021-02-20 19:58:16 +0900

    タイトル画面の外形の作成

---
+++ Lab/Assets/Scripts/LauncherScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class LauncherScript : MonoBehaviourPunCallbacks
+{
+    //スタート時に呼び出される
+    public void Connect(){
+        if(!PhotonNetwork.IsConnected){
+            PhotonNetwork.ConnectUsingSettings();//接続
+            Debug.Log("接続しました");
+        }
+    }
+
+    //コールバック
+    public override void OnConnectedToMaster(){
+        Debug.Log("OnConnectedToMasterが呼ばれました");
+        // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
+        PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
+    }
+
+    public override void OnJoinedRoom(){
+        Debug.Log("入室しました");
+        PhotonNetwork.LoadLevel("Molcar");//int型だけどいいのかな
+    }
+
+}
---
+++ Lab/Assets/Scripts/StartSceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSceneManager : MonoBehaviour
+{
+    bool enterflag = false;
+    [SerializeField] GameObject launcherObject;
+    LauncherScript launch;
+
+    void Start(){
+        launch = launcherObject.GetComponent<LauncherScript>();
+    }
+    void Update(){
+        if(!enterflag){
+            if(Input.GetKeyDown(KeyCode.Return)){
+                //launch.Connect();
+                enterflag = true;
+            }
+        }
+    }
+}

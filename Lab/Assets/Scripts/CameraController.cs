35e3d59ac026a1265dc2129ce3e1a70496127bf3
hama1185 <hamataku0120@gmail.com>
2021-02-23 04:57:53 +0900

    切り替え機能の実装

---
+++ Lab/Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject Player;
+    public GameObject frontCamera;
+    public GameObject backCamera;
+    public GameObject upCamera;
+    void Start(){
+        Player = GameObject.FindWithTag("Player");
+        frontCamera = Player.transform.GetChild(2).gameObject;
+        backCamera = Player.transform.GetChild(3).gameObject;
+
+        frontCamera.SetActive(false);
+        upCamera.SetActive(false);
+    }
+
+    public void changeCamera(){
+        if(backCamera.activeSelf){
+            frontCamera.SetActive(true);
+            backCamera.SetActive(false);
+        }
+        else if(frontCamera.activeSelf){
+            upCamera.SetActive(true);
+            frontCamera.SetActive(false);
+        }
+        else{
+            backCamera.SetActive(true);
+            upCamera.SetActive(false);
+        }
+    }
+}
---
+++ Lab/Assets/Scripts/GameInputManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameInputManager : MonoBehaviour
+{
+    CameraController cameraController;
+    // Start is called before the first frame update
+    void Start(){
+        cameraController = this.GetComponent<CameraController>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if(Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift)){
+            cameraController.changeCamera();
+        }
+    }
+}

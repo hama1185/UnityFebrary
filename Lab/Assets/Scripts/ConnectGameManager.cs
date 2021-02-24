2dd7be1e5293664caefb03eb5c78b95aa27169a4
hama1185 <hamataku0120@gmail.com>
2021-02-24 16:09:59 +0900

    キャラクター選択ができていることを確認

--- Lab/Assets/Scripts/ConnectGameManager.cs
+++ Lab/Assets/Scripts/ConnectGameManager.cs
@@ -8,7 +8,9 @@ using Photon.Realtime;
 public class ConnectGameManager : MonoBehaviourPunCallbacks
 {
     CameraController cameraController;
-    public GameObject playerPrefab;//あとでここを変更
+    //public GameObject playerPrefab;//あとでここを変更
+    string createName;
+    
     void Start(){
         if(!PhotonNetwork.IsConnected){
             SceneManager.LoadScene("Launcher");
@@ -17,7 +19,18 @@ public class ConnectGameManager : MonoBehaviourPunCallbacks
         //カメラスクリプトを取得
         cameraController = this.GetComponent<CameraController>();
         //自分のプレイヤーを生成
-        GameObject player = PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 1f, 0f), Quaternion.identity, 0);
+        if(SelectViewScript.selectCharactor == "random"){
+            if(Random.value >= 0.5f){
+                createName = "poteto";
+            }
+            else{
+                createName = "siromo";
+            }
+        }
+        else{
+            createName = SelectViewScript.selectCharactor;
+        }
+        GameObject player = PhotonNetwork.Instantiate(createName, new Vector3(0f, 1f, 0f), Quaternion.identity, 0);
         player.tag = "Player";
 
         Movement movement = player.GetComponent<Movement>();

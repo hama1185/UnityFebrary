b97b0c99dd10efc9ddd858a35ec8aa2b8dfd35da
hama1185 <hamataku0120@gmail.com>
2021-02-24 14:34:08 +0900

    画面の配置 動作の確認

---
+++ Lab/Assets/Scripts/SelectViewScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectViewScript : MonoBehaviour
+{
+    [SerializeField] GameObject potetoImage;
+    [SerializeField] GameObject siromoImage;
+    [SerializeField] GameObject questionImage;
+    // Start is called before the first frame update
+    public static string selectCharactor = "random";
+
+    public void OnChangedValuePoteto(){
+        potetoImage.SetActive(true);
+        siromoImage.SetActive(false);
+        questionImage.SetActive(false);
+        selectCharactor = "poteto";
+    }
+
+    public void OnChangedValueSiromo(){
+        potetoImage.SetActive(false);
+        siromoImage.SetActive(true);
+        questionImage.SetActive(false);
+        selectCharactor = "siromo";
+    }
+
+    public void OnChangedValueRandom(){
+        potetoImage.SetActive(false);
+        siromoImage.SetActive(false);
+        questionImage.SetActive(true);
+        selectCharactor = "random";
+    }
+}

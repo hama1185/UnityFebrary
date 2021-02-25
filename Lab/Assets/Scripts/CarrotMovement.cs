a4fbd52bf71799184fef58b358834dcd4e6d8697
hama1185 <hamataku0120@gmail.com>
2021-02-25 14:04:58 +0900

    try catch

---
+++ Lab/Assets/Scripts/CarrotMovement.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using UniRx;
+using UniRx.Triggers;
+
+public class CarrotMovement : MonoBehaviour
+{
+    public PhotonView myPV;
+    public PhotonTransformView myPTV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision){
+        if(!myPV.IsMine){
+            return;
+        }
+        if(collision.gameObject.tag == "Player" || collision.gameObject.tag == "OtherPlayer"){
+            try{
+                Observable.Timer(TimeSpan.FromMilliseconds(1000)).Subscribe(_ => PhotonNetwork.Destroy(this.gameObject));
+            }catch(MissingReferenceException){}
+        }
+    }
+}
